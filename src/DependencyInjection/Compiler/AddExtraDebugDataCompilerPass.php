c8b5a5ceb584964db399bcabc61d2cf02a794b2a
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2026-03-25 15:04:03 +0100

    Add CompilerPass that uses HelloWorldBundle DataCollector

---
+++ src/DependencyInjection/Compiler/AddExtraDebugDataCompilerPass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace IDCI\Bundle\DummyBundle\DependencyInjection\Compiler;
+
+use IDCI\Bundle\HelloWorldBundle\DataCollector\HelloWorldDataCollector;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class AddExtraDebugDataCompilerPass implements CompilerPassInterface
+{
+    public const EXTRA_DATA = [
+        [
+            'label' => 'Dummy',
+            'value' => 'Ici',
+        ],
+        [
+            'label' => 'Message',
+            'value' => 'Hello from Dummy !'
+        ]
+    ];
+
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->has(HelloWorldDataCollector::class)) {
+            return;
+        }
+
+        $helloWorldDataCollectorDefinition = $container->findDefinition(HelloWorldDataCollector::class);
+
+        foreach (self::EXTRA_DATA as $data) {
+            $helloWorldDataCollectorDefinition->addMethodCall('addExtraData', [$data]);
+        }
+    }
+}
